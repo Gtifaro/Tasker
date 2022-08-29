e0b63485034b34034ee603c424b104fa1f0f127a
Gabriel Rodríguez <gabrieltifaro2021@gmail.com>
2022-08-28 19:56:13 -0500

    Generate all the basic components to the future app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import React from "react";
+import {TodoCounter} from "./components/TodoCounter";
+import {TodoSearch} from "./components/TodoSearch";
+import {TodoList} from "./components/TodoList";
+import {TodoItem} from "./components/TodoItem";
+import {AddTodo} from "./components/AddTodo";
 
 function App() {
+
+  var Todos = [
+    {text: "Comer", finished: false},
+    {text: "Dormir", finished: false}
+  ]
+
   return (
-    <></>
+    <React.Fragment>
+      <TodoCounter todos={Todos} />
+      <TodoSearch/>
+      <TodoList>
+        {Todos.map(todos => (
+          <TodoItem key={todos.text} text={todos.text} finished={todos.finished}/>
+        ))}
+      </TodoList>
+      <AddTodo/>
+    </React.Fragment>
   );
 }
 
